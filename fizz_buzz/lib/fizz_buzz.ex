5979fb82ab0496dca1a115334b1079ed542a32af
Luan <luan.bernardes@tino.com.br>
2024-03-30 13:30:29 -0300

    numbers file convert to integer

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -1,10 +1,17 @@
 defmodule FizzBuzz do
   def build(file_name) do
     file_name
+    # pipe operator
     |> File.read()
     |> handle_file_read()
   end
 
-  def handle_file_read({:ok, result}), do: result
+  # pattern match
+  def handle_file_read({:ok, result}) do
+    result
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+
   def handle_file_read({:error, reason}), do: reason
 end
